eda686b69cc1397edaee91d12ebfdc8fcd6817b9
_ <soadedeji1@gmail.com>
2023-02-24 01:11:05 +0000

    scenarios

---
+++ 2-23-23/scenarios.py
@@ -0,0 +1,15 @@
+# Intro
+print("Hello world")
+# Pick a story
+
+  # Story 1: Display Story
+
+  # Story 1: Ask question
+
+    # Story 1: Good Ending
+
+    # Story 1: Bad Ending
+
+  # Story 2: Display Story
+
+  # Story 3: Display Story
